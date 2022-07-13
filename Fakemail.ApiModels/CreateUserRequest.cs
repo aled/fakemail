4b06017cef5dfafd85a25969c834f9b5e19c50dc
aled <aled@wibblr.com>
2022-07-13 19:25:20 +0100

    Remove unused usings

--- Fakemail.ApiModels/CreateUserRequest.cs
+++ Fakemail.ApiModels/CreateUserRequest.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fakemail.ApiModels
 {
