f9a4284b9d263ca3b0aac5483f8c4d30b6bf07bd
aled <aled@wibblr.com>
2024-07-20 23:07:18 +0100

    Add 'copy to clipboard' for host/smtp username/smtp password

--- Fakemail.Web/wwwroot/js/site.js
+++ Fakemail.Web/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+document.querySelectorAll('.copyable-text').forEach(item => {
+    item.addEventListener('click', () => copyTextToClipboard(item));
+});
+
+function copyTextToClipboard(element) {
+    const el = document.createElement('textarea');
+    el.value = element.innerText;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+}
